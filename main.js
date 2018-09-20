b4708ee98eba3363725915708a86c3ae050c02f3
Paulius Ke <37064988+Pauliuske@users.noreply.github.com>
2018-09-21 00:31:46 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log( " Labas ");
+window.addEventListener('load', () => document.querySelector
+('.preloader').classList.add('hide-preloader'))
+
+const Events = (()=>{
+  const navbar = document.querySelector('.navbar');
+  //show-hide preloader
+  // window.addEventListener('load', () => document.querySelector
+  // ('.preloader').classList.add('hide-preloader'))
+
+  //add class to navbar
+    window.addEventListener('scroll',() => {
+      let height = document.documentElement.scrollTop||window.pageOffset;
+
+      (height>76) ? navbar.classList.add('navbar-change')
+      : navbar.classList.remove('navbar-change');
+      ;
+    })
+
+})();
